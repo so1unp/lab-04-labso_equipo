be586abdb23754d08278a50cd9a0369ef4544ff8
mae2033 <matiasaepullan@lgmail.com>
2025-05-14 14:22:42 +0000

    buf refactoring

--- buf.c
+++ buf.c
@@ -35,7 +35,7 @@ static void* producer(void *p)
     int i = 0;
 
     struct params *params = (struct params*) p;
-    printf("Entra en el productor\n");
+    
     for (i = 0; i < params->items; i++) {
         
         sem_wait(&empty);
@@ -61,7 +61,7 @@ static void* consumer(void *p)
 
     // Reserva memoria para guardar lo que lee el consumidor.
     int *reader_results = (int*) malloc(sizeof(int)*params->items);
-    printf("Entra el consumidor\n");
+    
     for (i = 0; i < params->items; i++) {
         sem_wait(&full);
         pthread_mutex_lock(&mutex);
